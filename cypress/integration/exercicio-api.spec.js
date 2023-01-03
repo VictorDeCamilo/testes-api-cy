ea35df7dc1c8c51d6cf6996a11b2a246aa0e13e4
Victor De Camilo <victordecamilo@gmail.com>
2023-01-03 14:37:57 -0300

    Realizacao do exercicio do modulo 14

--- cypress/integration/exercicio-api.spec.js
+++ cypress/integration/exercicio-api.spec.js
@@ -1,30 +1,79 @@
 /// <reference types="cypress" />
+import contrato from '../contracts/produtos.contract'
 
 describe('Testes da Funcionalidade Usuários', () => {
 
+     let token
+
+     before(() => {
+          cy.token('fulano@qa.com', 'teste').then(tkn => { token = tkn })
+     });
+
     it('Deve validar contrato de usuários', () => {
-         //TODO: 
+     cy.request('usuarios').then(res=>{
+          return contrato.usersSchema.validateAsync(res.body)
+     })
     });
 
     it('Deve listar usuários cadastrados', () => {
-         //TODO: 
+     cy.request('usuarios').then(res=>{
+          expect(res.status).to.eq(200)
+          expect(res.body).to.not.be.empty
+          expect(res.body.quantidade).to.be.greaterThan(0)
+          expect(res.body.usuarios).to.have.length(res.body.quantidade)
+     })
     });
 
     it('Deve cadastrar um usuário com sucesso', () => {
-         //TODO: 
+     let email = Math.floor(Math.random() * 200 + 1) + '@teste.com'
+
+     cy.cadastrarUsuario(token, 'Teste', email, 'teste', 'true').then(res=>{
+          expect(res.status).to.eq(201)
+          expect(res.body.message).to.eq('Cadastro realizado com sucesso')
+          expect(res.body._id).to.not.be.null
+     })
     });
 
     it('Deve validar um usuário com email inválido', () => {
-         //TODO: 
+     let email = Math.floor(Math.random() * 200 + 1) + '@teste.com'
+
+     cy.cadastrarUsuario(token, 'Teste', email, 'teste', 'true').then(res=>{
+          expect(res.status).to.eq(201)
+          expect(res.body.message).to.eq('Cadastro realizado com sucesso')
+          expect(res.body._id).to.not.be.null
+          cy.cadastrarUsuario(token,'Teste', email, 'teste', 'true').then(res2=>{
+               expect(res2.status).to.eq(400)
+               expect(res2.body.message).to.eq('Este email já está sendo usado')
+          })
+     })
     });
 
     it('Deve editar um usuário previamente cadastrado', () => {
-         //TODO: 
+     let email = Math.floor(Math.random() * 200 + 1) + '@teste.com'
+     let email2 = Math.floor(Math.random() * 200 + 1) + '@teste.com'
+
+     cy.cadastrarUsuario(token, 'Teste', email, 'teste', 'true').then(res=>{
+          expect(res.status).to.eq(201)
+          expect(res.body.message).to.eq('Cadastro realizado com sucesso')
+          expect(res.body._id).to.not.be.null
+          cy.editarUser(token, res.body._id, 'Teste', email2, 'teste', 'true').then(res2=>{
+               expect(res2.status).to.eq(200)
+               expect(res2.body.message).to.eq('Registro alterado com sucesso')
+          })
+     })
     });
 
     it('Deve deletar um usuário previamente cadastrado', () => {
-        //TODO: 
+     let email = Math.floor(Math.random() * 200 + 1) + '@teste.com'
+
+     cy.cadastrarUsuario(token, 'Teste', email, 'teste', 'true').then(res=>{
+          expect(res.status).to.eq(201)
+          expect(res.body.message).to.eq('Cadastro realizado com sucesso')
+          expect(res.body._id).to.not.be.null
+          cy.deletarUser(token, res.body._id).then(res2=>{
+               expect(res2.status).to.eq(200)
+               expect(res2.body.message).to.eq('Registro excluído com sucesso')
+          })
+     })
     });
-
-
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,28 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 Cypress.Commands.add('token', (email, senha) => {
     cy.request({
         method: 'POST',
@@ -51,3 +26,47 @@ Cypress.Commands.add('token', (email, senha) => {
           failOnStatusCode: false
     })
  })
+
+Cypress.Commands.add('cadastrarUsuario', (token, nome, email, senha, administrador)=>{
+    cy.request({
+        method: 'POST',
+        url: 'usuarios',
+        headers: {
+            authorization: token
+        },
+        body:{
+            "nome": nome,
+            "email": email,
+            "password": senha,
+            "administrador": administrador
+        },
+        failOnStatusCode: false
+    })
+})
+
+Cypress.Commands.add('editarUser', (token, id, nome, email, senha, administrador) =>{
+    cy.request({
+        method: 'PUT',
+        url: `usuarios/${id}`,
+        headers: {
+            authorization: token
+        },
+        body:{
+            "nome": nome,
+            "email": email,
+            "password": senha,
+            "administrador": administrador
+        },
+        failOnStatusCode: false
+    })
+})
+
+Cypress.Commands.add('deletarUser', (token, id) => {
+    cy.request({
+        method: 'DELETE',
+        url: `usuarios/${id}`,
+        headers:{
+            authorization: token
+        }
+    })
+})
